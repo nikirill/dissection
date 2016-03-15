539efa223ce701886139c3004c4e57ea294f7539
Kirill Nikitin <kirill.nikitin@epfl.ch>
2016-03-15 16:24:43 +0100

    Added draft implementation of the algorithm

---
+++ algorithm.py
@@ -0,0 +1,55 @@
+import numpy as np
+from itertools import chain, combinations
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Complexity limit for a subset
+limit = 2
+
+G = nx.Graph()
+G.add_nodes_from(['A', 'B1', 'B2', 'B3', 'B4', 'Kb', 'C1', 'C2', 'C3', 'C4', 'Kc', 'D'])
+
+# Indicate degree of freedom for every vertex
+nx.set_node_attributes(G, 'df', {'A':1, 'B1':2, 'B2':2, 'B3':2, 'B4':2, 'Kb':1, 'C1':2, 'C2':2, 
+									'C3':2, 'C4':2, 'Kc':1, 'D':1})
+
+# Decribe edges in the graph
+G.add_edges_from([('A','B1'), ('A','B2'), ('A','B3'), ('A','B4'), ('B1','Kb'), ('B2','Kb'),
+				 ('B3','Kb'), ('B4','Kb'), ('B1','C1'), ('B2','C2'), ('B3','C3'), ('B4','C4'), 
+				 ('C1','Kc'), ('C2','Kc'), ('C3','Kc'), ('C4','Kc'), ('C1','D'), ('C2','D'), 
+				 ('C3','D'), ('C4','D')])
+
+# nx.draw(G)
+# plt.show()
+# Generate all possible subsets of vertices
+subsets = chain.from_iterable(combinations(G.nodes(), r) for r in range(1, len(G.nodes()) + 1))
+subgrs = []
+
+# Find all possible joints with defined complexity
+for subset in subsets:
+	sbgr = nx.subgraph(G, subset)
+	complexity = 0
+	for vertex in sbgr.nodes():
+		complexity += sbgr.node[vertex]['df']
+	complexity -= sbgr.number_of_edges()
+	if complexity <= limit:
+		subgrs.append(sbgr)
+
+
+for i in range (2, G.number_of_nodes()+1):  #levels of the tree
+	pairs = list(combinations(filter(lambda x: x.number_of_nodes()<i, subgrs), 2)) #all possible pairs below the level
+	for h in filter(lambda igr: igr.number_of_nodes()==i, subgrs): #all subsets on the level
+		reachability = False
+		for pair in pairs:
+			if (sorted(pair[0].nodes() + pair[1].nodes()) == sorted(h.nodes())):
+				reachability = True
+		if reachability == False:
+			subgrs.remove(h)
+		elif i == G.number_of_nodes():
+			print("Complexity = ", limit, "is achievable for key recovery")
+
+
+
+for ver in subgrs:
+	print(ver.nodes())
+
