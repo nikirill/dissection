b858b727d7ff6f51b4de22b12d44a51ccc339d88
Kirill Nikitin <kirill.nikitin@epfl.ch>
2016-05-19 10:59:18 +0200

    New

---
+++ 4graph_reduced.gml
@@ -0,0 +1,142 @@
+graph [
+  node [
+    id 0
+    label "C4"
+    df 2
+  ]
+  node [
+    id 1
+    label "B3"
+    df 2
+  ]
+  node [
+    id 2
+    label "Kc"
+    df 1
+  ]
+  node [
+    id 3
+    label "B4"
+    df 2
+  ]
+  node [
+    id 4
+    label "Kb"
+    df 1
+  ]
+  node [
+    id 5
+    label "B1"
+    df 2
+  ]
+  node [
+    id 6
+    label "D"
+    df 1
+  ]
+  node [
+    id 7
+    label "C3"
+    df 2
+  ]
+  node [
+    id 8
+    label "B2"
+    df 2
+  ]
+  node [
+    id 9
+    label "C1"
+    df 2
+  ]
+  node [
+    id 10
+    label "A"
+    df 1
+  ]
+  node [
+    id 11
+    label "C2"
+    df 2
+  ]
+  edge [
+    source 0
+    target 2
+  ]
+  edge [
+    source 0
+    target 6
+  ]
+  edge [
+    source 0
+    target 3
+  ]
+  edge [
+    source 1
+    target 7
+  ]
+  edge [
+    source 1
+    target 10
+  ]
+  edge [
+    source 1
+    target 4
+  ]
+  edge [
+    source 2
+    target 9
+  ]
+  edge [
+    source 2
+    target 7
+  ]
+  edge [
+    source 2
+    target 11
+  ]
+  edge [
+    source 3
+    target 10
+  ]
+  edge [
+    source 3
+    target 4
+  ]
+  edge [
+    source 4
+    target 5
+  ]
+  edge [
+    source 4
+    target 8
+  ]
+  edge [
+    source 5
+    target 9
+  ]
+  edge [
+    source 5
+    target 10
+  ]
+  edge [
+    source 6
+    target 9
+  ]
+  edge [
+    source 6
+    target 7
+  ]
+  edge [
+    source 6
+    target 11
+  ]
+  edge [
+    source 8
+    target 11
+  ]
+  edge [
+    source 8
+    target 10
+  ]
+]
--- fullgraph.py
+++ fullgraph.py
@@ -9,30 +9,30 @@ except:
 
 
 G = nx.Graph()
-# G.add_nodes_from(['A', 'B1', 'B2', 'B3', 'B4', 'Kb', 'C1', 'C2', 'C3', 'C4', 'Kc', 'D'])
-
-# # Indicate degree of freedom for every vertex
-# nx.set_node_attributes(G, 'df', {'A':1, 'B1':2, 'B2':2, 'B3':2, 'B4':2, 'Kb':1, 'C1':2, 'C2':2, 
-# 									'C3':2, 'C4':2, 'Kc':1, 'D':1})
-
-# # Decribe edges in the graph
-# G.add_edges_from([('A','B1'), ('A','B2'), ('A','B3'), ('A','B4'), ('B1','Kb'), ('B2','Kb'),
-# 				 ('B3','Kb'), ('B4','Kb'), ('B1','C1'), ('B2','C2'), ('B3','C3'), ('B4','C4'), 
-# 				 ('C1','Kc'), ('C2','Kc'), ('C3','Kc'), ('C4','Kc'), ('C1','D'), ('C2','D'), 
-# 				 ('C3','D'), ('C4','D')])
-
-G.add_nodes_from(['A1', 'A2', 'A3', 'A4', 'Ka', 'B1', 'B2', 'B3', 'B4', 'Kb', 'C1', 'C2', 'C3', 'C4', 'Kc', 'D'])
+G.add_nodes_from(['A', 'B1', 'B2', 'B3', 'B4', 'Kb', 'C1', 'C2', 'C3', 'C4', 'Kc', 'D'])
 
 # Indicate degree of freedom for every vertex
-nx.set_node_attributes(G, 'df', {'A1':1, 'A2':1, 'A3':1, 'A4':1, 'Ka':1, 'B1':2, 'B2':2, 'B3':2, 'B4':2, 'Kb':1, 'C1':2, 'C2':2, 
+nx.set_node_attributes(G, 'df', {'A':1, 'B1':2, 'B2':2, 'B3':2, 'B4':2, 'Kb':1, 'C1':2, 'C2':2, 
 									'C3':2, 'C4':2, 'Kc':1, 'D':1})
 
 # Decribe edges in the graph
-G.add_edges_from([('A1','B1'), ('A2','B2'), ('A3','B3'), ('A4','B4'), ('A1','Ka'), ('A2','Ka'),
-				 ('A3','Ka'), ('A4','Ka'), ('B1','Kb'), ('B2','Kb'),
+G.add_edges_from([('A','B1'), ('A','B2'), ('A','B3'), ('A','B4'), ('B1','Kb'), ('B2','Kb'),
 				 ('B3','Kb'), ('B4','Kb'), ('B1','C1'), ('B2','C2'), ('B3','C3'), ('B4','C4'), 
 				 ('C1','Kc'), ('C2','Kc'), ('C3','Kc'), ('C4','Kc'), ('C1','D'), ('C2','D'), 
 				 ('C3','D'), ('C4','D')])
+
+# G.add_nodes_from(['A1', 'A2', 'A3', 'A4', 'Ka', 'B1', 'B2', 'B3', 'B4', 'Kb', 'C1', 'C2', 'C3', 'C4', 'Kc', 'D'])
+
+# # Indicate degree of freedom for every vertex
+# nx.set_node_attributes(G, 'df', {'A1':1, 'A2':1, 'A3':1, 'A4':1, 'Ka':1, 'B1':2, 'B2':2, 'B3':2, 'B4':2, 'Kb':1, 'C1':2, 'C2':2, 
+# 									'C3':2, 'C4':2, 'Kc':1, 'D':1})
+
+# # Decribe edges in the graph
+# G.add_edges_from([('A1','B1'), ('A2','B2'), ('A3','B3'), ('A4','B4'), ('A1','Ka'), ('A2','Ka'),
+# 				 ('A3','Ka'), ('A4','Ka'), ('B1','Kb'), ('B2','Kb'),
+# 				 ('B3','Kb'), ('B4','Kb'), ('B1','C1'), ('B2','C2'), ('B3','C3'), ('B4','C4'), 
+# 				 ('C1','Kc'), ('C2','Kc'), ('C3','Kc'), ('C4','Kc'), ('C1','D'), ('C2','D'), 
+# 				 ('C3','D'), ('C4','D')])
 
 # nx.write_gml(G, '4graph_reduced.gml')
 
